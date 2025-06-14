2bb1a2ab956f4f9f40281c718eb87223e1767bfb
fahshed <f.morshed.077@gmail.com>
2025-06-14 01:45:33 -0400

    bottomtabs setup done

---
+++ app/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,23 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import AddEntryScreen from "../screens/AddEntryScreen";
+import HomeScreen from "../screens/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        // headerStyle: { backgroundColor: "#0047AB" },
+        // headerTintColor: "#fff",
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Add Entry" component={AddEntryScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
